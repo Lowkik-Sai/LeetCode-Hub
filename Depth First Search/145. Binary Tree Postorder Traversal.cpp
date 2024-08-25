ad106667a6b7d7a52bb5a2fc8c81f5cbd22a853a
Lowkik Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2024-08-25 09:59:50 +0530

    Problem Difficulty Level : Easy

---
+++ Depth First Search/145. Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,35 @@
+    /*
+    Time complexity : O(n)
+    Space complexity : O(n)
+    */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void traverse(TreeNode* root, vector<int>& res){
+        if(!root) return;
+        if(root->left) traverse(root->left, res);
+        if(root->right) traverse(root->right, res);
+        res.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        traverse(root, res);
+        return res;
+    }
+};
+
+    /*
+    Author : Lowkik-Sai
+    Question Link : hhttps://leetcode.com/problems/binary-tree-postorder-traversal/description/?envType=daily-question&envId=2024-08-25
+    */
