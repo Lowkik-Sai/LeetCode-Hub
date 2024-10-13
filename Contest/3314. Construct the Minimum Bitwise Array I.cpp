88d216acf61cc4e82df058af82b0224d9ac286aa
Lowkik-Sai <lowkiksaipotnuru@gmail.com>
2024-10-13 17:18:02 +0530

    Problem Difficulty Level : Easy

---
+++ Contest/3314. Construct the Minimum Bitwise Array I.cpp
@@ -0,0 +1,31 @@
+
+    /*
+    Time complexity : O(n * max(nums))
+    Space complexity : O(n)
+    */
+
+
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> res;
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 0; j < nums[i]; j++){
+                if((j | (j + 1)) == nums[i]){
+                    res.push_back(j);
+                    break;
+                }
+            }
+            if(res.size() == i) res.push_back(-1);
+        }
+        return res;
+    }
+};
+
+
+    /*
+    Author : Lowkik-Sai
+    Question Link : https://leetcode.com/problems/construct-the-minimum-bitwise-array-i/
+    */
+
+    
